fe8c5913485c7954e707987c25b48ba947bb4369
uknomzaying <121236013+Anzuniks@users.noreply.github.com>
2026-02-05 10:02:08 +0200

    Korjattu

--- Module4/src/osa4_1/Task1/Main.java
+++ Module4/src/osa4_1/Task1/Main.java
@@ -1,4 +1,18 @@
 package osa4_1.Task1;
 
+
 public class Main {
+    //Lets test our calulator//
+    public static void main(String [] args) {
+        Calculator calculator = new Calculator();
+        calculator.add(5);
+        calculator.add(10);
+//      calculator.add(-5); //This should give an exception//
+
+        System.out.println("Current value: " + calculator.getValue());
+
+        calculator.reset();
+
+        System.out.println("Value after reset: " + calculator.getValue());
+    }
 }
