8175fda5ba12623f877f5561b929818f7663a7ba
uknomzaying <121236013+Anzuniks@users.noreply.github.com>
2026-02-05 10:01:58 +0200

    Korjattu

--- Module4/src/osa4_1/Task1/Calculator.java
+++ Module4/src/osa4_1/Task1/Calculator.java
@@ -1,4 +1,28 @@
 package osa4_1.Task1;
 
 public class Calculator {
+    private int value = 0;
+
+
+    //Method that returns the current value//
+    public int getValue() {
+        return value;
+    }
+
+    //Method that resets the value to zero//
+    public void reset() {
+        value = 0;
+    }
+
+    //Importan method that adds a number and checks the negativity//
+    public void add(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Only positive numbers are allowed");
+        }
+        this.value += number;
+    }
 }
+
+
+
+
